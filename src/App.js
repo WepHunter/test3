f5dd193ffbd5a3c65314c0f5fb245c530b843d7f
WepHunter <43216510+WepHunter@users.noreply.github.com>
2023-02-08 18:03:30 +0000

    Division

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="https://images.stopgame.ru/news/2022/07/06/blI--S-.jpg" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          To protect the World<span className="heart">♥️</span>
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://thedivision.fandom.com/ru/wiki/%D0%92%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D0%B8_%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA%D0%B0#%D0%A1%D1%8E%D0%B6%D0%B5%D1%82"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            forward to win
           </a>
         </p>
       </header>
